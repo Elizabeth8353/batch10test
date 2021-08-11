74ac7f826d28072e071278312f3567c81d0f7b95
Elizabeth8353 <evaldivieso6@gmail.com>
2021-08-11 19:21:20 -0400

    First commit eclipse project

---
+++ src/gitproject/Gitclass.java
@@ -0,0 +1,11 @@
+package gitproject;
+
+public class Gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first git comit");
+
+	}
+
+}
