e9de158cc1d6340824e0e7af1bc204ef3031a6e3
Elizabeth8353 <evaldivieso6@gmail.com>
2021-08-11 19:54:39 -0400

    modified secound class

--- src/gitproject/SecoundGit.java
+++ src/gitproject/SecoundGit.java
@@ -5,6 +5,7 @@ public class SecoundGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("second commit");
+System.out.println("hoeifhjkv");
 	}
 
 }
